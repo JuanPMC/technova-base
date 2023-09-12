3a329093fcdabfe892e6894d1987048536b9145f
Juan <juan.technova@gmail.com>
2023-09-12 08:11:29 -0500

    Updated for static files

--- proyectobase/settings.py
+++ proyectobase/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = "static/"
-STATIC_ROOT = os_path.join(BASE_DIR, "static")
+STATIC_ROOT = os_path.join(BASE_DIR,"dist","static")
 MEDIA_URL = "media/"
 MEDIA_ROOT = os_path.join(BASE_DIR, "media")
 
